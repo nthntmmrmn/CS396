1ee263683375bfdd34e781f1d321af760821cb24
nthntmmrmn <netimmerman@gmail.com>
2020-01-22 19:12:19 -0600

    testing file

--- tester.py
+++ tester.py
@@ -9,10 +9,20 @@ if not os.path.exists(path):
 
 df_bus = pd.read_json(path+'/business.json', lines=True)
 closed = df_bus[df_bus['is_open']==0].shape[0]
+closed_ids = closed['business_id'].to_numpy()
 print(f'{closed} / {df_bus.shape[0]} = {closed / df_bus.shape[0]}')
 
-df_rev = pd.read_json(path+'/review.json', lines=True)
-df_rev = df_rev[df_bus["buisness_id"] == closed]['rating'].count
+ilbus = []
+for l in open(path+"/review.json").readlines():
+	data = json.loads(l)
+	if data["business_id"] in closed_ids:
+		ilbus.append(data)
+
+df_rev = pd.DataFrame.from_records(ilbus)
+print(df_rev['rating'].count)
+
+# df_rev = pd.read_json(path+'/review.json', lines=True)
+# df_rev = df_rev[df_bus["v"] == closed]['rating'].count
 # df_MIA = df_tip[df_tip['business_id'] == business_id] # get all tips for McCarran Intl Airport
 # dates = df_MIA['date'].dt.year.to_numpy() # get the hour of each date of the tips for MIA
 # values, counts = np.unique(dates, return_counts=True) # find how many tips were left at each hour
